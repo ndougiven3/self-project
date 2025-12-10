69157f54485a68f5e5717584ac4b481778f8ace6
Given Ndou <given@synthesis.co.za>
2025-12-10 10:49:37 +0200

    remediated the security findings

--- main.tf
+++ main.tf
@@ -102,7 +102,7 @@ resource "aws_s3_bucket_public_access_block" "amplify_wheelspin" {
 
   block_public_acls       = true
   ignore_public_acls      = true
-  block_public_policy     = true
+  block_public_policy     = false
   restrict_public_buckets = true
 }
 // ...existing code...
